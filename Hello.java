a358a8e3bc5c4d00a718b985ab494ff9207462d0
hyb <459840110@qq.com>
2020-07-08 22:49:51 +0800

    day 2 Add Hello.java method add

--- Hello.java
+++ Hello.java
@@ -1,5 +1,10 @@
 public class Hello{
 	public static void main(String[] args){
 		System.out.println("第一天只写了一个主函数");
+		System.out.println("结果:"+add(100,200));
+	}
+	public static int add(int a,int b){
+		System.out.println("第二天做了一个加法器");
+		return a+b;
 	}
 }	
